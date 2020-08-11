e9e8fca3177c14a85918b3b3e7a5572d6b2ae71b
Nathan Kinkade <nkkinkade@gmail.com>
2020-08-11 15:59:01 -0500

    getting caught up

---
+++ sql/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE IF NOT EXISTS userstates (
+  id SERIAL PRIMARY KEY,
+  stateName VARCHAR(50),
+  positive VARCHAR(50),
+  negative VARCHAR(50),
+  hospitalizedCurrently VARCHAR(50),
+  recovered VARCHAR(50),
+  death VARCHAR(50),
+  totalTest VARCHAR(50),
+  positiveTests VARCHAR(50),
+  negativeTests VARCHAR(50)
+);
