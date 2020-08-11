a0c843cb681de5c9ad867de96145d856c2c53a83
ChristianLievrouw <celievrouw1@gmail.com>
2020-08-11 11:38:05 -0500

    added constructor function

--- server.js
+++ server.js
@@ -4,6 +4,7 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const superagent = require('superagent');
 
 // Application Setup
 const app = express();
@@ -15,6 +16,7 @@ app.use(express.urlencoded({ extended: true }));
 
 // Route Definitions
 app.get('/', rootHandler);
+app.get('/states', getStateData);
 app.use('*', notFoundHandler);
 app.use(errorHandler);
 
@@ -22,11 +24,31 @@ app.use(errorHandler);
 function rootHandler(request, response) {
   response.status(200).send('COVID Watcher Backend');
 }
+
+function getStateData(request, response) {
+  const url = 'https://api.covidtracking.com/v1/states/current.json';
+  superagent.get(url)
+  console.log(response)
+}
+
 function notFoundHandler(request, response) {
   response.status(404).json({ notFound: true });
 }
+
 function errorHandler(error, request, response, next) {
   response.status(500).json({ error: true, message: error.message });
+}
+
+function States(state){
+  this.stateName = state.state;
+  this.positive = state.positive;
+  this.negative = state.negative;
+  this.hospitalizedCurrently = state.hospitalizedCurrently;
+  this.recovered = state.recovered;
+  this.death = state.death;
+  this.totalTest = state.totalTestsViral;
+  this.positiveTests = state.positiveTestsViral;
+  this.negativeTests = state.negativeTestsViral;
 }
 
 // App listener
