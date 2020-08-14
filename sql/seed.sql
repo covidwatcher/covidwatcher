662e1cbdae158ca23dd664087bbf0834b0b4aa9e
Nathan Kinkade <nkkinkade@gmail.com>
2020-08-14 14:50:20 -0500

    add footer and styled about page

--- sql/seed.sql
+++
@@ -1,2 +0,0 @@
-INSERT INTO userstates ("stateName", positive, negative, "hospitalizedCurrently", recovered, death, "totalTest", "positiveTests", "negativeTests")
-VALUES ('IOWA', '100', '200', '300', '400', '500', '600', '700', '800');
